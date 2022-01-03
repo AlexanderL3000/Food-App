4e4504f39b7f02cf5ca0922781449e9d49ce2ce0
etanguan <guanethan123@gmail.com>
2022-01-02 23:35:37 -0500

    Added sell button to test sell page, fixed bugs with sell page

--- FoodApp/app/src/main/java/com/example/foodapp/MainActivity.java
+++ FoodApp/app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.foodapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,14 +17,19 @@ import com.example.foodapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -32,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+
     }
 
 }
--- FoodApp/app/src/main/java/com/example/foodapp/ui/dashboard/DashboardFragment.java
+++ FoodApp/app/src/main/java/com/example/foodapp/ui/dashboard/DashboardFragment.java
@@ -1,9 +1,11 @@
 package com.example.foodapp.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,11 +14,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.foodapp.MainActivity;
 import com.example.foodapp.R;
+import com.example.foodapp.SellActivity;
 import com.example.foodapp.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
-
+    private Button sell;
     private DashboardViewModel dashboardViewModel;
     private FragmentDashboardBinding binding;
 
@@ -35,7 +39,26 @@ public class DashboardFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        // Example button to lead to sell page
+        sell = (Button) binding.SellButton;
+        sell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSellActivity();
+            }
+        });
+
         return root;
+
+
+    }
+
+    // Example function for opening activity
+    public void openSellActivity() {
+        Intent intent = new Intent(DashboardFragment.this.getActivity(), SellActivity.class);
+        startActivity(intent);
+
     }
 
     @Override

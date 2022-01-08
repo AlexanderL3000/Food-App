2136ca4e1f312f46687892e1c33504967cfb2168
etanguan <guanethan123@gmail.com>
2022-01-08 01:00:08 -0500

    Started Sign in/register

--- FoodApp/app/src/main/java/com/example/foodapp/MainActivity.java
+++ FoodApp/app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.foodapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -35,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_account)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
---
+++ FoodApp/app/src/main/java/com/example/foodapp/ui/account/AccountFragment.java
@@ -0,0 +1,38 @@
+package com.example.foodapp.ui.account;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.foodapp.databinding.FragmentAccountBinding;
+
+public class AccountFragment extends Fragment {
+
+    private AccountViewModel accountViewModel;
+    private FragmentAccountBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        accountViewModel =
+                new ViewModelProvider(this).get(AccountViewModel.class);
+
+        binding = FragmentAccountBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}

610e9c60fc5a0829d2c80b26a06e95b74dedff90
etanguan <guanethan123@gmail.com>
2022-05-13 21:15:51 -0400

    now uploads url of pic

--- FoodApp/app/src/main/java/com/example/foodapp/SellActivity.java
+++ FoodApp/app/src/main/java/com/example/foodapp/SellActivity.java
@@ -1,19 +1,31 @@
 package com.example.foodapp;
-
+import android.os.Handler;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.ProgressBar;
+import android.media.Image;
+import android.net.UrlQuerySanitizer;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.canhub.cropper.CropImageActivity;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 import android.Manifest;
 import android.app.AlertDialog;
@@ -27,6 +39,8 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.ContentResolver;
+import android.webkit.MimeTypeMap;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -36,6 +50,7 @@ import androidx.core.content.ContextCompat;
 import com.squareup.picasso.Picasso;
 import com.canhub.cropper.CropImage;
 
+import java.net.URL;
 
 
 public class SellActivity extends AppCompatActivity implements View.OnClickListener {
@@ -43,16 +58,24 @@ public class SellActivity extends AppCompatActivity implements View.OnClickListe
     private String seller;
     private Button sellBtn;
     private EditText title, price, quantity, description;
-    Uri URI;
+    Uri imageUri;
     private FirebaseAuth mAuth;
     FirebaseDatabase rootNode;
     DatabaseReference reference;
+    private StorageReference mStorageRef;
     private DatabaseReference mDatabase;
+    private DatabaseReference mDatabaseRef;
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference ref = database.getReference("server/saving-data/fireblog");
+
+    String imageUrl;
+
     private String hi;
+    private StorageTask mUploadTask;
 
     ImageButton uploadPic;
+    private ProgressBar mProgressBar;
+
 
 
 
@@ -69,7 +92,11 @@ public class SellActivity extends AppCompatActivity implements View.OnClickListe
         price = findViewById(R.id.SellPriceInput);
         quantity = findViewById(R.id.SellQuantityInput);
         description = findViewById(R.id.SellDescriptionInput);
+
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
+
 
 
         uploadPic = findViewById(R.id.picUpload);
@@ -93,7 +120,53 @@ public class SellActivity extends AppCompatActivity implements View.OnClickListe
         super.onActivityResult(requestCode, resultCode, data);
         Uri uri = data.getData();
         uploadPic.setImageURI(uri);
-        URI = uri;
+        imageUri = uri;
+        uploadFile();
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    private void uploadFile() {
+        if (imageUri != null) {
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+                    + "." + getFileExtension(imageUri));
+
+            mUploadTask = fileReference.putFile(imageUri)
+
+
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+
+
+                            Toast.makeText(SellActivity.this, "Upload Complete", Toast.LENGTH_LONG).show();
+                            fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+
+                                    final String downloadUrl =
+                                            uri.toString();
+                                    Upload upload = new Upload(title.getText().toString().trim(),downloadUrl);
+
+                                    imageUrl = downloadUrl;
+
+                                    String uploadId = mDatabaseRef.push().getKey();
+                                    mDatabaseRef.child(uploadId).setValue(upload);
+                                }
+                            });
+                            /**Upload upload = new Upload(title.getText().toString().trim(),
+                                    taskSnapshot.getDownloadURL().toString());
+
+                            String uploadId = mDatabaseRef.push().getKey();
+                            mDatabaseRef.child(uploadId).setValue(upload);*/
+                        }
+                    });
+        }
     }
 
     @Override
@@ -107,15 +180,19 @@ public class SellActivity extends AppCompatActivity implements View.OnClickListe
 
     }
     private void sellItem() {
+
+
+
         String seller = mAuth.getCurrentUser().getUid();
         String Title = title.getText().toString().trim();
         String Price = price.getText().toString().trim();
         String Quantity = quantity.getText().toString().trim();
         String Description = description.getText().toString().trim();
-        String uri = URI.toString();
 
 
-        SellItem item = new SellItem(Title, Price, Quantity, Description, seller, uri);
+
+
+        SellItem item = new SellItem(Title, Price, Quantity, Description, seller, imageUrl);
         DatabaseReference Ref = ref.child("SellItems");
         DatabaseReference newRef = Ref.push();
         newRef.setValue(item);
--- FoodApp/app/src/main/java/com/example/foodapp/SellItem.java
+++ FoodApp/app/src/main/java/com/example/foodapp/SellItem.java
@@ -1,18 +1,18 @@
 package com.example.foodapp;
 
 public class SellItem {
-    public String title, price, quantity, description, seller, uri;
+    public String title, price, quantity, description, seller, imageUrl;
 
     public SellItem() {
 
     }
 
-    public SellItem(String title,String price, String quantity, String description, String seller, String uri) {
+    public SellItem(String title,String price, String quantity, String description, String seller, String imageUrl) {
         this.title = title;
         this.price = price;
         this.quantity = quantity;
         this.description = description;
         this.seller = seller;
-        this.uri = uri;
+        this.imageUrl = imageUrl;
     }
 }
